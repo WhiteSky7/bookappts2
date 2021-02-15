006ac36c996bb5471fe294a309723477ef1de08a
WhiteSky7 <satalk77@gmail.com>
2021-02-15 19:04:20 +0300

    Update router tsx

--- frontend/router.tsx
+++ frontend/router.tsx
@@ -2,12 +2,14 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import {BrowserRouter , Switch, Route} from 'react-router-dom';
-import {Page} from 'components/Page'
+import {Index} from 'components/Index'
 
 export const RouterComponent = hot(module)(() => (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Page} />
+        <Route path="/" component={Index} />
       </Switch>
+      
     </BrowserRouter>
+    
 ));
