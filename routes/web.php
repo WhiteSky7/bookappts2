6e6927d3a90d4f3650ae793d169c3a77cb371d1e
WhiteSky7 <satalk77@gmail.com>
2021-02-16 08:47:07 +0300

    Add routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+Route::get('/book', 'App\Http\Controllers\BookController@index');
+Route::get('/book/index', 'App\Http\Controllers\BookController@index');
+Route::get('/book/list', 'App\Http\Controllers\BookController@index');
+Route::get('/book/form', 'App\Http\Controllers\BookController@index');
+Route::get('/book/edit/{num}', 'App\Http\Controllers\BookController@index');
+
+Route::get('/lib', 'App\Http\Controllers\LibController@index');
+Route::get('/lib/index', 'App\Http\Controllers\LibController@index');
+Route::get('/lib/list', 'App\Http\Controllers\LibController@index');
+Route::get('/lib/form', 'App\Http\Controllers\LibController@index');
+Route::get('/lib/edit/{num}', 'App\Http\Controllers\LibController@index');
+
+
 
 Route::view('/{all?}', 'app')
     ->where(['all' => '.*']);
