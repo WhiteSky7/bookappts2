8c8c31e4de92d96c5dd509c1196f819d826aab2e
WhiteSky7 <satalk77@gmail.com>
2021-02-16 03:01:27 +0300

    Add models for entites

---
+++ app/Models/Lib.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lib extends Model
+{
+    use HasFactory;
+}
